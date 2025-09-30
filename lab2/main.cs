ff1ff413905e6e6cbbacee80179f8e8e64062539
meier <lllkojiotp9c@gmail.com>
2025-10-01 02:08:43 +0300

    5

--- lab2/main.cs
+++ lab2/main.cs
@@ -109,15 +109,17 @@ class Game
                 if (!cat.GetState() == State.Ready)
                 {
                     Console.Writeline("Игрок " + cat.name + ", подтвердите готовность к игре, написав что-либо в консоль.");
+                    Console.Readline();
                     cat.SetSatate(State.Ready);
-                    //прочитать ходы кота из файла
+                    //прочитать ходы кота из файла в массив
                 }
 
                 if (!mouse.state == State.Ready)
                 {
                     Console.Writeline("Игрок " + mouse.name + ", подтвердите готовность к игре, написав что-либо в консоль.");
+                    Console.Readline();
                     mouse.SetState(State.Ready);
-                    //прочитать ходы мыши из файла
+                    //прочитать ходы мыши из файла в массив
                 }
 
                 if (mouse.GetState() == State.Ready && cat.GetState() == State.Ready)
@@ -129,12 +131,74 @@ class Game
                 }
             }
 
-            //движок
-
-            if (cat.GetState() == State.Looser || mouse.GetState == State.Looser)
+            int catTurns;
+            int mouseTurns;
+            Console.Writeline("Введите количество ходов для игрока кот: ");
+            catTurns = Console.Readline();
+            int[] catTurnsStorage = new int[catTurns];
+            for (int i = 0; i < catTurns; i++)
             {
-                Game.SetState(GameState.End);
+                Console.Writeline("Введите шаг игрока кот для хода + " + (i + 1));
+                catTurnsStorage[i] = Console.Readline();
             }
+            
+            Console.Writeline("Введите количество ходов для игрока мышь: ");
+            mouseTurns = Console.Readline();
+            int[] mouseTurnsStorage = new int[mouseTurns];
+            for (int i = 0; i < catTurns; i++)
+            {
+                Console.Writeline("Введите шаг игрока мышь для хода + " + (i + 1));
+                mouseTurnsStorage[i] = Console.Readline();
+            }
+
+            int globalSteps = catTurns;
+            if (globalSteps < mouseTurns)
+            {
+                globalSteps = mouseTurns;
+            }
+
+            for (int i = 0; i < globalSteps; i++)
+            {
+                if (!globalSteps > mouseTurns)
+                {
+                    mouse.Move(mouseTurnsStorage[i]);
+                }
+
+                if (!globalSteps > catTurns)
+                {
+                    cat.Move(catTurnsStorage[i]);
+                }
+
+                int currentDistance;
+                if (mouse.getLocation() > cat.getLOcation())
+                {
+                    currentDistance = mouse.getLocation() - cat.getLocation();
+                }
+
+                else if (mouse.getLocation() < cat.getLOcation())
+                {
+                    currentDistance = cat.getLocation() - mouse.getLocation();
+                }
+
+                else {
+                    mouse.setState(State.Looser);
+                    cat.setState(State.Winner);
+                }
+                
+
+                if (mouse.GetState == State.Looser)
+                {
+                    Console.Writeline("Игра окончена. Кот поймал мышь!");
+                    Game.SetState(GameState.End);
+                }
+
+                Console.Writeline("Расстояние между котом и мышью на ходу +" + (i + 1) + " равно " + currentDistance);
+            }
+
+            Console.Writeline("Игра окончена. Кот не смог поймать мышь!");
+            cat.setState(State.Looser);
+            Game.SetState(GameState.End);
+            
         }
     }
 }
