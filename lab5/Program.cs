2e3ef9c28dceb6de1927147e303fb560efeba364
meier <lllkojiotp9c@gmail.com>
2025-12-08 17:38:36 +0300

    3

--- lab5/Program.cs
+++ lab5/Program.cs
@@ -1,9 +1,4 @@
-﻿public interface IHumanable
-{
-	void GenerateRating();
-}
-
-public interface IMovable
+﻿public interface IMovable
 {
 	void SendToClient();
 
@@ -49,8 +44,9 @@ public abstract class Auto
 
 public abstract class Mercedes : Auto, IMovable
 {
+	public Client OrderClient { get; set; } = new Client();
 	public int ChanceOfGettingBetterRating { get; set; }
-	Mercedes(
+	public Mercedes(
 	int v,
 	int ep,
 	int ms,
@@ -58,11 +54,9 @@ public abstract class Mercedes : Auto, IMovable
 	int cf,
 	int ls,
 	string c,
-	bool ic,
-	int tuu,
 	string m,
 	string b,
-	int cogbr) : base(v, ep, ms, fc, cf, ls, c, ic, tuu, m, b)
+	int cogbr) : base(v, ep, ms, fc, cf, ls, c, m, b)
 	{
 		this.ChanceOfGettingBetterRating = cogbr;
 	}
@@ -75,43 +69,49 @@ public abstract class Mercedes : Auto, IMovable
 
 public abstract class Volvo : Auto, IMovable
 {
+	public Client OrderClient { get; set; } = new Client();
 	public int AdditionalSeats { get; set; }
-	Volvo(int v,
+	public Volvo(
+	int v,
 	int ep,
 	int ms,
 	int fc,
 	int cf,
 	int ls,
 	string c,
-	bool ic,
-	int tuu,
 	string m,
 	string b,
-	int ads) : base(v, ep, ms, fc, cf, ls, c, ic, tuu, m, b)
+	int ads) : base(v, ep, ms, fc, cf, ls, c, m, b)
 	{
 		this.AdditionalSeats = ads;
 	}
+
+	public override string ToString()
+	{
+		return "";
+	}
 	public void SendToClient()
 	{
-
+		Console.WriteLine("Автомобиль " + ToString() + "отправлен на вызов.");
+		//фокусы автомобиля и результат доставки клиента
 	}
 }
 
 public abstract class Lada : Auto, IMovable
 {
+	public Client OrderClient { get; set; } = new Client();
 	public int AdditionalLuggageSpace { get; set; }
-	Lada(int v,
+	public Lada(
+	int v,
 	int ep,
 	int ms,
 	int fc,
 	int cf,
 	int ls,
 	string c,
-	bool ic,
-	int tuu,
 	string m,
 	string b,
-	int als) : base(v, ep, ms, fc, cf, ls, c, ic, tuu, m, b)
+	int als) : base(v, ep, ms, fc, cf, ls, c, m, b)
 	{
 		this.AdditionalLuggageSpace = als;
 	}
@@ -123,19 +123,19 @@ public abstract class Lada : Auto, IMovable
 
 public abstract class Honda : Auto, IMovable
 {
+	public Client OrderClient { get; set; } = new Client();
 	public int ChanceOfFasterComplition { get; set; }
-	Honda(int v,
+	public Honda(
+	int v,
 	int ep,
 	int ms,
 	int fc,
 	int cf,
 	int ls,
 	string c,
-	bool ic,
-	int tuu,
 	string m,
 	string b,
-	int cofc) : base(v, ep, ms, fc, cf, ls, c, ic, tuu, m, b)
+	int cofc) : base(v, ep, ms, fc, cf, ls, c, m, b)
 	{
 		this.ChanceOfFasterComplition = cofc;
 	}
@@ -147,27 +147,61 @@ public abstract class Honda : Auto, IMovable
 
 public abstract class Geely : Auto, IMovable
 {
+	public Client OrderClient { get; set; } = new Client();
 	public int ChanceOfBreakDown { get; set; }
-	Geely(int v,
+	public Geely(
+	int v,
 	int ep,
 	int ms,
 	int fc,
 	int cf,
 	int ls,
 	string c,
-	bool ic,
-	int tuu,
 	string m,
 	string b,
-	int cobd) : base(v, ep, ms, fc, cf, ls, c, ic, tuu, m, b)
+	int cobd) : base(v, ep, ms, fc, cf, ls, c, m, b)
 	{
 		this.ChanceOfBreakDown = cobd;
 	}
 
+	public bool TryToRepair(Random r)
+	{
+		int max = 5;
+		int repairRandom = r.Next(max);
+
+		bool isRepaired;
+		if (repairRandom <= 2)
+		{
+			isRepaired = false;
+		}
+		else
+		{
+			isRepaired = true;
+		}
+
+		return isRepaired;
+
+
+	}
 	public bool Repair()
 	{
-		//сколько стоило
-		return true;
+		Random random = new Random();
+		int min = 500;
+		int max = 25000;
+		int repairValue = random.Next(min, max);
+
+		Console.WriteLine("Автомобиль сломался. Клиент недоволен. Стоимость ремонта: " + repairValue + ". Починить автомобиль? (Y/n)");
+		string choice = Console.ReadLine();
+		switch (choice)
+		{
+			case "n":
+				Console.WriteLine("Автомобиль требует ремонта. Клиент оставил негативный отзыв.");
+				return false;
+			default:
+				bool isRepaired = TryToRepair(random);
+				Console.WriteLine("Текущий бюджет:");
+				return isRepaired;
+		}
 	}
 	public void SendToClient()
 	{
@@ -177,39 +211,62 @@ public abstract class Geely : Auto, IMovable
 
 public enum Urgency
 {
+	Null,
 	NonUrgent,
 	Urgent,
 	VeryUrgent,
 
 }
 
-public class Client : IHumanable
+public class Client
 {
 	Urgency Urgency { get; set; }
 	public int Money { get; set; }
 	public int DistanceToTravel { get; set; }
 	public int GeneratedRating { get; set; }
 	public int Luggage { get; set; }
+
+
+	public Client()
+	{
+		this.Urgency = Urgency.Null;
+		this.Money = -1;
+		this.DistanceToTravel = -1;
+		this.Luggage = -1;
+
+	}
+	public Client(Urgency u, int m, int dtt, int l)
+	{
+		this.Urgency = u;
+		this.Money = m;
+		this.DistanceToTravel = dtt;
+		this.Luggage = l;
+	}
 }
 
-public struct Rating
+public class Rating
 {
 	public int NumOfRatings { get; set; }
 	public int AverageRating { get; set; }
 
-	Rating()
+	public Rating()
 	{
 		this.NumOfRatings = 0;
 		this.AverageRating = 0;
 	}
 }
-public class TaxiCompany()
+public class TaxiCompany
 {
 	public int Balance { get; set; }
-	public Rating rating { get; set; } = Rating();
+	public Rating Rating { get; set; } = new Rating();
 
 
-
+	public TaxiCompany(int b, int nor, int ar)
+	{
+		this.Balance = b;
+		this.Rating.NumOfRatings = nor;
+		this.Rating.AverageRating = ar;
+	}
 
 
 }
